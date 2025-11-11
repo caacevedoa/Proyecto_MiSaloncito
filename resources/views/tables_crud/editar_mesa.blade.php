0bbaa12860b3ef536df6b6ebd7e1ddd1cfa6ef74
Catalina Acevedo Arias <cata.acevedo2007@gmail.com>
2025-11-11 16:58:45 -0500

    Cambio nombre de un campo, estético

--- resources/views/tables_crud/editar_mesa.blade.php
+++ resources/views/tables_crud/editar_mesa.blade.php
@@ -10,10 +10,10 @@
     <form action="{{route('tables.update', $table->id)}}" method="post">
         @method('PUT')
         @csrf
-        <label for="table_number">Nombre:</label>
+        <label for="table_number">Número de mesa:</label>
         <input type="text" name="table_number" id="table_number" value="{{ $table->table_number }}"><br>
 
-        
+
         <label for="table_status">Seleccione el estado de la mesa:</label>
         <select name="table_status" id="table_status">
             <option value="libre">Libre</option>
